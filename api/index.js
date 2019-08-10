3909793b9e4bbdc3f68c7c0e1aa0672e19ca0151
rusian0 <sssdaphne@gmail.com>
2019-08-11 01:02:52 +0900

    dynamodb接続調整

--- api/index.js
+++ api/index.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = express()
 const AWS = require('aws-sdk')
 
+AWS.config.update({
+    credentials: new AWS.Credentials(
+        "acesskey",
+        "secretkey"
+      ),
+      region: "dynamo-admin:8002"
+  })
+
 const dynamodb = new AWS.DynamoDB({ endpoint: new AWS.Endpoint('http://dynamo-admin:8002') })
 
 app.get("/", function(req, res) {
