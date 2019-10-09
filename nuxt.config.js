02e1402c1e7aa6f2f7c939dcd6e65f1107acfe47
in <in@verocity.jp>
2019-10-09 20:12:44 +0900

    GA集計追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,7 @@ export default {
     {src: 'plugins/amplify.js',ssr: false},
     {src: 'plugins/vue-youtube.js',ssr: false},
     {src: 'plugins/vue-plyr.js',ssr: false},
+    {src: 'plugins/ga.js',ssr: false},
   ],
   /*
   ** Nuxt.js dev-modules
