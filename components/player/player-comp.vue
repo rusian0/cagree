3f4460b1dc8713bdc1a0760192e811dd1508fe28
pipipid <pipipd1753@gmail.com>
2020-02-14 00:53:11 +0900

    video queue スタイル修正

--- components/player/player-comp.vue
+++ components/player/player-comp.vue
@@ -18,71 +18,37 @@
             </div>
         </div>
         <div class="row player-body">
-            <div class="video-body col-12 col-sm-12 col-md-9 col-lg-9 table-cell">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="youtube-movie">
-                            <vue-plyr>
-                                <div class="plyr__video-embed">
-                                    <youtube
-                                        ref="youtube"
-                                        :video-id="queue_ids[0]"
-                                        :player-vars="playerVars"
-                                        @ready="ready"
-                                        @paused="paused"
-                                        @playing="playing"
-                                        @buffering="buffering"
-                                        @ended="ended"
-                                        @cued="cued"
-                                        @error="error"
-                                        width="1920"
-                                        height="1080"
-                                    />
-                                    <font-awesome-icon class="forward" icon="forward"/>
-                                </div>
-                            </vue-plyr> 
+            <div class="col-8">
+                <div class="youtube-movie">
+                    <vue-plyr>
+                        <div class="plyr__video-embed">
+                            <youtube
+                                ref="youtube"
+                                :video-id="queue_ids[0]"
+                                :player-vars="playerVars"
+                                @ready="ready"
+                                @paused="paused"
+                                @playing="playing"
+                                @buffering="buffering"
+                                @ended="ended"
+                                @cued="cued"
+                                @error="error"
+                                width="1920"
+                                height="1080"
+                            />
+                            <font-awesome-icon class="forward" icon="forward"/>
                         </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="icon py-2 float-right pr-1">
-                            {{ 100000 }} <font-awesome-icon class="icon" icon="user" />
-                        </div>
-                    </div>
+                    </vue-plyr> 
                 </div>
             </div>
-            <div class="comment-body col-0 col-sm-0 col-md-3 col-lg-3 table-cell">
-                <div class="row">
-                    <div class="col-12 comment-titile text-center pt-1" style="background-color: #920000; color: #FFFFFF;">
-                        <h5>chat</h5>
-                    </div>
-                    <div class="comment-text col-12">
-                        <div v-for="(n, i) in 100">
-                            guest_test{{ i }}: text{{ i }}<br>
+            <div class="col-4 queue-body">
+                <div class="queue-list">
+                    <draggable class="row" element="div" :options="options" v-model="queue_ids" @end="queueDragEnd">
+                        <div class="col-4 text-center" v-for="(queue_id, index) in queue_ids">
+                            <button @click="deleteQueue(index)" class="btn btn-danger queue_delete">✗</button>
+                            <img :src="imgUrl + queue_id + '/mqdefault.jpg'" @click="selectQueue(index)" alt="">
                         </div>
-                    </div>
-                    <div class="col-12 comment-input input-group">
-                        <input type="text" maxlength="300" class="form-control" v-model="comments">
-                        <span class="input-group-append">
-                            <button type="button" class="btn">
-                                <font-awesome-icon class="mb-1" :class="{'icon-primary': comments }" icon="paper-plane"/>
-                            </button>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row queue-body">
-            <div class="col-12">
-                <div style="background-color:black;padding:20px;">
-                    <h3 style="color:white">Video queue</h3>
-                    <ul class="queue-list">
-                        <draggable :options="options" v-model="queue_ids" @end="queueDragEnd">
-                            <li v-for="(queue_id, index) in queue_ids">
-                                <button @click="deleteQueue(index)" class="btn btn-danger queue_delete">✗</button>
-                                <img :src="imgUrl + queue_id + '/mqdefault.jpg'" @click="selectQueue(index)" alt="">
-                            </li>    
-                        </draggable> 
-                    </ul>
+                    </draggable> 
                 </div>
             </div>
         </div>
