52bec1c9f651371eceee88b7a003265272ac09b6
in <in@verocity.jp>
2020-03-14 02:59:45 +0900

    スタイル調整

--- components/player/player-comp.vue
+++ components/player/player-comp.vue
@@ -5,7 +5,7 @@
             </div> -->
             <div class="col-11 col-lg-7 col-xl-5">
                 <div class="input-group url-play">
-                    <input class="form-control url-input" type="text" v-model="video_url" v-on:keydown.enter="url_play" placeholder="Youtube動画のURLを貼り付けて右のボタンを押してください">
+                    <input class="form-control url-input" type="text" v-model="video_url" v-on:keydown.enter="url_play" placeholder="ここにYoutube動画のURLを貼り付けて右のボタンを押してください">
                     <div>
                         <button class="btn btn-url-primary play" @click="url_play('force')"><font-awesome-icon icon="play" /></button>
                     </div>
@@ -42,7 +42,8 @@
             </div>
             <div class="col-12 col-sm-12 col-md-12 col-lg-4">
                 <div class="queue-body text-center">
-                    <draggable class="row" element="div" :options="options" v-model="queue_ids" @end="queueDragEnd">
+                    <p class="queue_desc">サムネイル画像を掴んで移動させたり<br>クリックすると再生の順番を変更できます</p>
+                    <draggable class="row queue_imgs" element="div" :options="options" v-model="queue_ids" @end="queueDragEnd">
                         <div class="col-3 col-sm-2 col-md-2 col-lg-4 text-center" v-for="(queue_id, index) in queue_ids">
                             <button @click="deleteQueue(index)" class="queue_delete"><font-awesome-icon icon="times-circle" /></button>
                             <img :src="imgUrl + queue_id + '/mqdefault.jpg'" @click="selectQueue(index)" alt="">
