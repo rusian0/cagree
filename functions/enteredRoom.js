c579176aec1631699e94325fe5c30f3ca238f8d4
in <in@verocity.jp>
2020-03-16 23:08:51 +0900

    入室時ファンクション調整

--- functions/enteredRoom.js
+++ functions/enteredRoom.js
@@ -16,24 +16,33 @@ const now = moment().format('YYYY-MM-DD-HH:mm:ss')
 module.exports = functions.region('asia-northeast1').runWith(runtimeOpts).https.onCall(async (data, context) => {
     
     const auth = context.auth ? context.auth : null
+
+    console.log('auth check')
     
-    if(auth === null || !auth.uid || auth.token.provider_id !== 'anonymous') return null
-    
+    if(auth === null || !auth.uid || auth.token.provider_id !== 'anonymous') {
+        console.log('no auth')
+        return 'no auth'
+    }
+    console.log('auth ok')
     const userPowerCol = db.collection('userPower')
 
+    console.log('uid:', auth.uid)
     const userPowerDocRef = userPowerCol.doc(auth.uid)
     const userPowerDoc = await userPowerDocRef.get()
 
+    console.log('add power')
     // ユーザー権限の初期データが作成されていなければ作成
     if(!userPowerDoc.exists) {
         await userPowerDocRef.set({ 
             createdAt: now,
             ownerRooms: [auth.uid]
         })
-    }
-    else {
-        userPowerDocRef.update({ ownerRooms: admin.firestore.FieldValue.arrayUnion(data.roomId) })
+        console.log('add power doc')
     }
 
-    return null
+    console.log('powerdocData', userPowerDoc.data())
+    userPowerDocRef.update({ ownerRooms: admin.firestore.FieldValue.arrayUnion(data.roomId) })
+    console.log('update power doc')
+
+    return 'done'
 })
