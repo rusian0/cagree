f3209b906d7379ba703877aabd66fd0072e52c36
in <in@verocity.jp>
2020-03-25 19:19:49 +0900

    firebase ga 実装

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -6,11 +6,14 @@ const config = {
     databaseURL: process.env.FB_DATABASE_URL,
     projectId: process.env.FB_PROJECTID,
     storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
-}
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_APP_ID,
+    measurementId: process.env.FB_MEA_ID
+}   
 
 if (!firebase.apps.length) {
     firebase.initializeApp(config)
+    firebase.analytics()
 }
 
 export default firebase
