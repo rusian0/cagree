b1cbb5b87db29c6c1df96bd61bd3e9496dede5f7
in <in@verocity.jp>
2020-04-02 23:45:17 +0900

    ogp画像を指定しないパターンを追加

--- functions/generateMeta.js
+++ functions/generateMeta.js
@@ -44,25 +44,28 @@ module.exports = functions.runWith(runtimeOpts).https.onRequest(async (req, res)
     ogTitle.setAttribute('content', 'PulScreen | Youtube動画のリアルタイム共有サービス')
     head.appendChild(ogTitle)
 
-    const ogImage = doc.createElement('meta')
-    ogImage.setAttribute('property', 'og:image')
-    ogImage.setAttribute('content', 'https://firebasestorage.googleapis.com/v0/b/dev-pulscreen.appspot.com/o/common%2Fmain.jpg?alt=media&token=29aa58c1-0362-4a3b-ba12-7799853cc757')
-    head.appendChild(ogImage)
 
-    const ogDesc = doc.createElement('meta')
-    ogDesc.setAttribute('property', 'og:description')
-    ogDesc.setAttribute('content', '異なったデバイス同士でも。複数人で同じ動画を同じタイミングで視聴できる。YouTube動画のリアルタイム共有サービス「PulScreen(プルスクリーン)」')
-    head.appendChild(ogDesc)
-
-    const twitterCard = doc.createElement('meta')
-    twitterCard.setAttribute('name', 'twitter:card')
-    twitterCard.setAttribute('content', 'summary_large_image')
-    head.appendChild(twitterCard)
-
-    const twitterSite = doc.createElement('meta')
-    twitterSite.setAttribute('name', 'twitter:site')
-    twitterSite.setAttribute('content', '@Rusian0')
-    head.appendChild(twitterSite)
+    if(req.query.noshare === undefined || req.query.noshare !== undefined && req.query.noshare === false){
+        const ogImage = doc.createElement('meta')
+        ogImage.setAttribute('property', 'og:image')
+        ogImage.setAttribute('content', 'https://firebasestorage.googleapis.com/v0/b/dev-pulscreen.appspot.com/o/common%2Fmain.jpg?alt=media&token=29aa58c1-0362-4a3b-ba12-7799853cc757')
+        head.appendChild(ogImage)
+        
+        const ogDesc = doc.createElement('meta')
+        ogDesc.setAttribute('property', 'og:description')
+        ogDesc.setAttribute('content', '異なったデバイス同士でも。複数人で同じ動画を同じタイミングで視聴できる。YouTube動画のリアルタイム共有サービス「PulScreen(プルスクリーン)」')
+        head.appendChild(ogDesc)
+        
+        const twitterCard = doc.createElement('meta')
+        twitterCard.setAttribute('name', 'twitter:card')
+        twitterCard.setAttribute('content', 'summary_large_image')
+        head.appendChild(twitterCard)
+        
+        const twitterSite = doc.createElement('meta')
+        twitterSite.setAttribute('name', 'twitter:site')
+        twitterSite.setAttribute('content', '@Rusian0')
+        head.appendChild(twitterSite)
+    }
 
     // const meta = document.querySelector('meta[data-hid="description"]')
     // meta.setAttribute('content', 'testdescription')
